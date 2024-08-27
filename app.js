d6e2363d1dcdce6a1e30d54f473e13250830eb37
Shea Syve <syversonshea@gmail.com>
2024-08-26 19:24:03 -0600

    Button changes and styling. Also software Proficiency section.

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-document.getElementById("theButton").addEventListener("click", function() {
-    alert("This incident will be reported.");
+let count = 0;
+const countElement = document.getElementById('count');
+const button = document.getElementById('theButton');
+button.addEventListener("click", function() {
+    count += 1; 
+    if(count == 1){
+        alert(`You have pressed the button 1 time.`);
+    }
+    else if (count > 9){
+        alert(`You have pressed the button ${count} times. Are you ok?`);
+    }
+    else{
+        alert(`You have pressed the button ${count} times.`);
+    }
+    countElement.textContent = count;
 });
